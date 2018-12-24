5597cd8505f8e9c6f673fc37598ae7d5fb65ca76
dannyshin0510 <shindannybms@gmail.com>
2018-12-24 02:40:24 -0500

    AI portion made

--- script.js
+++ script.js
@@ -23,7 +23,13 @@ function startGame()
 
 function turnClick(square)
 {
-	turn (square.target.id, huPlayer)
+	if (typeof origBoard[square.target.id]=='number')
+	{
+		turn (square.target.id, huPlayer)
+	if (!checkTie()) 
+		turn (bestSpot(), aiPlayer);
+	}
+	
 
 }
 function turn (squareId, player)
@@ -58,4 +64,34 @@ return gameWon;
  			cells[i].removeEventListener('click', turnClick, false);
  		}
  	}
+ 	declareWinner(gameWon.player==huPlayer ? "You win!":"You lose!");
  }
+
+ function declareWinner (who)
+ {
+ 	document.querySelector(".endgame").style.display="block";
+ 	document.querySelector(".endgame .text").innerText=who;
+ }
+function emptySquares()
+{
+	return origBoard.filter(s => typeof s=='number');
+}
+ function bestSpot()
+ {
+ 	return emptySquares()[0];
+ }
+
+ function checkTie()
+ {
+if (emptySquares().length==0)
+{
+	for (var i=0; i<cells.length; i++)
+	{
+		cells[i].style.backgroundColor="purple";
+		cells[i].removeEventListener('click', turnClick, false);
+	}
+	declareWinner ("It's a TIE!")
+	return true;
+ }
+ return false;
+}
